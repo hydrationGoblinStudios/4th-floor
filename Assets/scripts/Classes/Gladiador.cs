9cc33e7ba574950fdc8e96919d34c54f27137cdb
SuperMegaGamerVerdadeiro <bernardobressan10@gmail.com>
2023-02-24 17:54:07 -0300

    gladiador e prisioneiro
    super homem

---
+++ Assets/scripts/Classes/Gladiador.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gladiador : UnitBehavior
+{
+    public override int Soul(int damage)
+    {
+        if (enemy)
+        {
+            hp += damage / 2;
+            StartCoroutine(battleManager.PlayerExtraAttack("Super golpe"));
+            battleManager.PlayerBar -= 50;
+            battleManager.StatChange();
+        }
+        else
+        {
+            hp += damage / 2;
+            StartCoroutine(battleManager.PlayerExtraAttack("Super Golpe"));
+            battleManager.EnemyBar -= 50;
+            battleManager.StatChange();
+        }
+
+        return 0;
+    }
+    public override int Proc(int damage)
+    {
+
+        if ((battleManager.enemyBehavior.maxhp - battleManager.enemyBehavior.hp)/100 > 50)
+        {
+            //critico n ta aparecendo
+            crit += 15;
+            battleManager.StatChange();
+            return 0;
+        }
+        return 0;
+    }
+}
