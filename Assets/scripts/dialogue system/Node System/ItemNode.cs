2bec117350f6c51007bca1c4c3a1970eb7d2a217
hydrationGoblinStudios <raphael.vconsidera@al.infnet.edu.br>
2023-04-04 15:25:39 -0300

    item node

---
+++ Assets/scripts/dialogue system/Node System/ItemNode.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemNode : BaseNode
+{
+    [Input] public int entry;
+    [Output] public int exit;
+    public Item item;
+    public bool key;
+    private GameManager Manager;
+    private GameObject GameManagerOBJ;
+    public override string GetString()
+    {
+        GameManagerOBJ = GameObject.FindGameObjectWithTag("game manager");
+        Manager = GameManagerOBJ.GetComponent<GameManager>();
+        if (key)
+        {
+            Manager.KeyItems.Add(item);
+        }
+        else
+        {
+            Manager.Inventory.Add(item);
+        }
+        return "itemNode/";
+    }
+}
