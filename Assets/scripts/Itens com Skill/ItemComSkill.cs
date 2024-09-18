18439330807a0f2167a671d4bb9c8b122ce06f7a
SuperMegaGamerVerdadeiro <bernardobressan10@gmail.com>
2024-09-18 16:59:07 -0300

    armas novas e coisas

---
+++ Assets/scripts/Itens com Skill/ItemComSkill.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "newItemcomSkill", menuName = "ItemComSkill")]
+public class ItemComSkill : Item
+{
+
+
+}
