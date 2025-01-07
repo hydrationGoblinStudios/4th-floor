bd00dfbf3297bf15e726483d90e82ef537f03c3c
hydrationGoblinStudios <raphael.vconsidera@al.infnet.edu.br>
2025-01-07 17:40:56 -0300

    keyItemParadoxController

---
+++ Assets/scripts/ItemSystem/KeyItemParadoxController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItemParadoxController : MonoBehaviour
+{
+    public DialogueGraph vazio;
+    public List<Item> items;
+    public List<GameObject> interactables;
+    public List<SpriteRenderer> sprites;
+    public GameManager manager;
+
+    public void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("game manager").GetComponent<GameManager>();
+        foreach (Item item in manager.KeyItems)
+        {
+        int c = 0;
+            foreach(GameObject i in interactables)
+            {
+                if(i.name == item.name + " Interactable")
+                {
+                    sprites[c].sprite = null;
+                    i.GetComponent<ButtonAssigner>().graph = vazio;
+                    i.GetComponent<ButtonAssigner>().AddListener();
+                }
+            c++;
+            }
+        }
+    }
+}
